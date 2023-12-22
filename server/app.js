2ded2d5839ae13fd5f9cc3a9ab4c03937a0753a3
Meitoc <info@meitoc.com>
2023-12-22 09:47:05 +0700

    worktask

--- server/app.js
+++ server/app.js
@@ -11,12 +11,12 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
 const mongoose = require("mongoose")
 /* DB connection*/
 const mongoURI = process.env.MONGODB_URI;
